516f2059ab70a753a3664885e1dc421b98c7cbe6
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2024-03-11 18:18:06 +0100

    removed unused code

--- GuessTheWordGame/src/mykyta/zemlianyi/guess/the/word/game/DatabaseManager.java
+++ GuessTheWordGame/src/mykyta/zemlianyi/guess/the/word/game/DatabaseManager.java
@@ -7,7 +7,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.sql.ResultSet;
 
 public class DatabaseManager {
--- GuessTheWordGame/src/mykyta/zemlianyi/guess/the/word/game/Main.java
+++ GuessTheWordGame/src/mykyta/zemlianyi/guess/the/word/game/Main.java
@@ -22,8 +22,6 @@ public class Main {
 	public static Word secretWord;
 	public static Word inputWord;
 
-//	 public static String inputWord;
-//	 public static char[] inputWordAsChar;
 
 	public static void main(String[] args) {
 		boolean GameWon = false;
